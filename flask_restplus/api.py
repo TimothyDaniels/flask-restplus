4dea108fe73fcd862b3edc55359f04a437e9ea34
rossjp <rossjp@users.noreply.github.com>
2017-11-17 15:18:53 -0500

    Make swagger.json path relative

--- flask_restplus/api.py
+++ flask_restplus/api.py
@@ -444,7 +444,7 @@ class Api(object):
 
         :rtype: str
         '''
-        return url_for(self.endpoint('specs'), _external=True)
+        return url_for(self.endpoint('specs'), _external=False)
 
     @property
     def base_url(self):
